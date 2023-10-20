d665c820ada6d923f26d45e48f3f222880c2f978
Lilian Cristine Semann <126900345+LilianSemann@users.noreply.github.com>
2023-10-20 10:12:02 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import Link from "next/link";
 export default function Home() {
   return (
     <section className="flex flex-col p-4">
-      <Link className="w-fit" href={"/1"}>First step - creating <span className="font-semibold">Arrays</span> and using <span className="font-semibold">.map</span></Link>
-      <Link className="w-fit" href={"/2"}>Second step - why should we use Typescript? What did we learn about <span className="font-semibold">Props</span></Link>
-      <Link className="w-fit" href={"/3"}>Third step - adding new things to an array (without actually atualizing the screen)</Link>
-      <Link className="w-fit" href={"/4"}>Fourth step - rendering screen: how and why to <span className="font-semibold">useState</span></Link>
-      <Link className="w-fit" href={"/5"}>Fifth step - going back to step three - updating userlist</Link>
-      <Link className="w-fit" href={"/6"}>Sixth step - changing text with input</Link>
+      <div className="bg-white p-5 rounded-2xl text-black flex flex-col gap-3">
+        <Link className="w-fit" href={"/1"}>First step - creating <span className="font-semibold">Arrays</span> and using <span className="font-semibold">.map</span></Link>
+        <Link className="w-fit" href={"/2"}>Second step - why should we use Typescript? What did we learn about <span className="font-semibold">Props</span></Link>
+        <Link className="w-fit" href={"/3"}>Third step - adding new things to an array (without actually atualizing the screen)</Link>
+        <Link className="w-fit" href={"/4"}>Fourth step - rendering screen: how and why to <span className="font-semibold">useState</span></Link>
+        <Link className="w-fit" href={"/5"}>Fifth step - going back to step three - updating userlist</Link>
+        <Link className="w-fit" href={"/6"}>Sixth step - changing text with input</Link>
+      </div>
     </section>
   )
 }
